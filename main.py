3a2050e56ac5b0f4405db629bc31589139b891f7
djarbuz25 <djdjarbuz25@gmail.com>
2024-02-03 17:17:36 +0700

    a

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request,):
+    return templates.TemplateResponse(
+        request=request, name="homework.html",
+    )
+uvicorn.run(app)
